3db08b4537a9c6205d491f4cc548a397153dbd6a
tracer-ics2020 <tracer@njuics.org>
2021-08-05 22:45:10 +0800

    >  compile 191220000    Static hostname: zj          Icon name: computer-laptop            Chassis: laptop         Machine ID: ed65ba0b9ff84c5ebba30b86475a4861            Boot ID: e1da79a96dbc4bacb85264b6ec14cd98   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.4.0-81-generic       Architecture: x86-64  22:45:10 up  3:15,  1 user,  load average: 0.86, 0.54, 0.42

--- nemu/src/isa/riscv32/exec/control.h
+++ nemu/src/isa/riscv32/exec/control.h
@@ -2,6 +2,7 @@ static inline def_EHelper(jal) {
     rtl_addi(s, ddest, &cpu.pc, 4);
     // rtlreg_t tmp = cpu.pc;
     rtl_addi(s, s0, &cpu.pc, id_src1->simm);
-    rtl_j(s, *s0);
+    printf("\033[43;34ms0 is %d\033[0m\n", *s0);
+    rtl_jr(s, s0);
     // printf("\033[43;34mold pc is 0x%x, simm is 0x%x, new pc is 0x%x\033[0m\n", tmp, id_src1->simm, cpu.pc);
 }
