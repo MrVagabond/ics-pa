bf64e3c124384759b3c4830213c18420c4f24d8f
tracer-ics2020 <tracer@njuics.org>
2021-08-05 20:28:36 +0800

    >  compile 191220000    Static hostname: zj          Icon name: computer-laptop            Chassis: laptop         Machine ID: ed65ba0b9ff84c5ebba30b86475a4861            Boot ID: e1da79a96dbc4bacb85264b6ec14cd98   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.4.0-81-generic       Architecture: x86-64  20:28:36 up 59 min,  1 user,  load average: 0.41, 0.48, 0.30

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -4,6 +4,10 @@
 #include "difftest.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  for(int i = 0; i < 31; i ++) {
+    printf("0x%x\n", ref_r->gpr[i]._32);
+  }
+  printf("pc: 0x%x", ref_r->pc);
   return false;
 }
 
