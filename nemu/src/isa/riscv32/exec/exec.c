01759c3935b7f45d988d83fb90e4828d2e914d3c
tracer-ics2020 <tracer@njuics.org>
2021-08-06 01:17:36 +0800

    >  compile 191220000    Static hostname: zj          Icon name: computer-laptop            Chassis: laptop         Machine ID: ed65ba0b9ff84c5ebba30b86475a4861            Boot ID: e1da79a96dbc4bacb85264b6ec14cd98   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.4.0-81-generic       Architecture: x86-64  01:17:36 up  5:48,  1 user,  load average: 0.51, 0.50, 0.53

--- nemu/src/isa/riscv32/exec/exec.c
+++ nemu/src/isa/riscv32/exec/exec.c
@@ -48,14 +48,14 @@ static inline def_EHelper(grp_01100) {
         EXW (0b0100000, sub, 4)
       }
       break;
-    EXW (0b001, sll, 4)
+    //EXW (0b001, sll, 4)
     EXW (0b010, slt, 4)
     EXW (0b011, sltu, 4)
     EXW (0b100, xor, 4)
     case 0b101:
       switch(s->isa.instr.r.funct7) {
-        EXW (0b0000000, srl, 4)
-        EXW (0b0100000, sra, 4)
+        //EXW (0b0000000, srl, 4)
+        //EXW (0b0100000, sra, 4)
       }
     EXW (0b110, or, 4)
     EXW (0b111, and, 4)
