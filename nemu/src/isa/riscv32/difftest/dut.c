29a3a95ab6d56baa11930ed65b8d15bd0341a8b9
tracer-ics2020 <tracer@njuics.org>
2021-08-06 13:41:07 +0800

    >  compile 191220000    Static hostname: zj          Icon name: computer-laptop            Chassis: laptop         Machine ID: ed65ba0b9ff84c5ebba30b86475a4861            Boot ID: e1da79a96dbc4bacb85264b6ec14cd98   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.4.0-81-generic       Architecture: x86-64  13:41:07 up 18:11,  1 user,  load average: 0.78, 0.55, 0.29

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -5,9 +5,14 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) { // 传入的是qemu的状态和nemu的新pc
   // 之所以ref_r的类型是nemu中的CPU_state，是因为获取qemu状态的函数参数为void *，后来被强转为CPU_state
+  int flagerr = 0;
   for(int i = 0; i < 31; i ++) {
-    Assert(ref_r->gpr[i]._32 == cpu.gpr[i]._32, "gpr[%d] not matched: in nemu is 0x%x while in qemu is 0x%x\n", i, cpu.gpr[i]._32, ref_r->gpr[i]._32);
+    if(ref_r->gpr[i]._32 != cpu.gpr[i]._32) {
+      flagerr = 1;
+      printf("gpr[%d] not matched: in nemu is 0x%x while in qemu is 0x%x\n", i, cpu.gpr[i]._32, ref_r->gpr[i]._32);
+    }
   }
+  assert(flagerr == 0);
   Assert(ref_r->pc == cpu.pc, "pc not matched: in nemu is 0x%x while in qemu is 0x%x\n", cpu.pc, ref_r->pc);
   return true;
 }
