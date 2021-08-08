22ad5ce6f0b36a4b45d309a9c0c7722c4890a976
tracer-ics2020 <tracer@njuics.org>
2021-08-08 15:33:38 +0800

    >  run 191220000    Static hostname: zj          Icon name: computer-laptop            Chassis: laptop         Machine ID: ed65ba0b9ff84c5ebba30b86475a4861            Boot ID: e1da79a96dbc4bacb85264b6ec14cd98   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.4.0-81-generic       Architecture: x86-64  15:33:38 up 2 days, 20:04,  1 user,  load average: 0.40, 0.51, 0.45

--- abstract-machine/am/src/nemu/ioe/input.c
+++ abstract-machine/am/src/nemu/ioe/input.c
@@ -6,10 +6,13 @@
 #include <klib.h>
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keycode = inl(KBD_ADDR);
-  if(kbd->keycode & KEYDOWN_MASK) {
-    kbd->keydown = true;
-  } else {
-    kbd->keydown = false;
-  }
+  // kbd->keycode = inl(KBD_ADDR);
+  // if(kbd->keycode & KEYDOWN_MASK) {
+  //   kbd->keydown = true;
+  // } else {
+  //   kbd->keydown = false;
+  // }
+  int k = inl(KBD_ADDR);
+  kbd->keydown = (k & KEYDOWN_MASK ? true : false);
+  kbd->keycode = k & ~KEYDOWN_MASK;
 }
