cfdd223aa99f9f90eb325d90f8af48c654f9a01f
tracer-ics2020 <tracer@njuics.org>
2021-08-08 17:02:52 +0800

    >  run 191220000    Static hostname: zj          Icon name: computer-laptop            Chassis: laptop         Machine ID: ed65ba0b9ff84c5ebba30b86475a4861            Boot ID: e1da79a96dbc4bacb85264b6ec14cd98   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.4.0-81-generic       Architecture: x86-64  17:02:52 up 2 days, 21:33,  1 user,  load average: 1.38, 0.79, 0.54

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -21,6 +21,8 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   };
 }
 
+#define min(a,b) ((a)<(b)?(a):(b))
+
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     // 在(x,y)坐标处画一个w*h的矩形，像素按行优先存储在pixels中，每个像素用32位整数00RRGGBB的方式描述颜色
     // int x = ctl->x, y = ctl->y;
@@ -42,23 +44,20 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     // if(ctl->sync) {
     //   outl(SYNC_ADDR, 1);
     // }
-    int x = ctl->x, y = ctl->y;
-    void *pixels = ctl->pixels;
-    int w = ctl->w, h = ctl->h;
-
-    assert(0 <= x && x + h < inw(VGACTL_ADDR)); // 矩形的行坐标应该在高度的范围内
-    assert(0 <= y && y + w < inw(VGACTL_ADDR + 2)); // 矩形的列坐标应该在宽度的范围内
-    
-    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-    int i = 0;
-    for(int row = x; row < x + h; row ++) {
-      for(int col = y; col < y + w; col ++) {
-        *(fb + row * inw(VGACTL_ADDR + 2) + col) = ((uint32_t *)pixels)[i];
-        i ++;
-      }
+    uint32_t *fb = (uint32_t*)(uintptr_t)FB_ADDR;
+    int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+    uint32_t *pixels = ctl->pixels;
+    uint16_t max_h = inl(VGACTL_ADDR);
+    uint16_t max_w = inl(VGACTL_ADDR + 2);
+    int cp_bytes = sizeof(uint32_t) * min(w, max_w - x);
+    // Copy the bytes to fb in row first.
+    for (int i = 0; i < h && i + y < max_h; i ++) {
+      memcpy(&fb[(y + i) * max_w + x], pixels, cp_bytes);
+      // for (int j )
+      pixels += w;
     }
-    assert(i == w * h);
-    if(ctl->sync) {
+
+    if (ctl->sync) {
       outl(SYNC_ADDR, 1);
     }
 }
--- abstract-machine/klib/include/klib-macros.h
+++ abstract-machine/klib/include/klib-macros.h
@@ -12,6 +12,7 @@
 #define _CONCAT(x, y)       x ## y
 #define CONCAT(x, y)        _CONCAT(x, y)
 
+
 #define putstr(s) \
   ({ for (const char *p = s; *p; p++) putch(*p); })
 
