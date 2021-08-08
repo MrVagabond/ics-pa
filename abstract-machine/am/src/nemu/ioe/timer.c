2d5ef6160613d595a07a7681ba3bec352acc2140
tracer-ics2020 <tracer@njuics.org>
2021-08-08 13:26:18 +0800

    >  run 191220000    Static hostname: zj          Icon name: computer-laptop            Chassis: laptop         Machine ID: ed65ba0b9ff84c5ebba30b86475a4861            Boot ID: e1da79a96dbc4bacb85264b6ec14cd98   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.4.0-81-generic       Architecture: x86-64  13:26:18 up 2 days, 17:56,  1 user,  load average: 0.46, 0.51, 0.30

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -9,22 +9,25 @@ static uint32_t __am_timer_init_secs;
 void __am_timer_init() {
   __am_timer_init_usecs = inl(RTC_ADDR);
   __am_timer_init_secs = inl(RTC_ADDR + 4);
+  
   //printf("initial time %u secs and %u usecs\n", __am_timer_init_secs, __am_timer_init_usecs);
-  puts(utoa(__am_timer_init_usecs));
-  putch('\n');
-  puts(utoa(__am_timer_init_secs));
-  putch('\n');
+  
+  //puts(utoa(__am_timer_init_usecs));
+  //putch('\n');
+  //puts(utoa(__am_timer_init_secs));
+  //putch('\n');
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint32_t current_usecs = inl(RTC_ADDR);
   uint32_t current_secs = inl(RTC_ADDR + 4);
   uptime->us = (current_secs - __am_timer_init_secs) * 1000000 - __am_timer_init_usecs + current_usecs;
-  //uptime->us = 100;
+  
   //printf("__am_timer_uptime us = %u\n", uptime->us);
-  puts("total usecs is ");
-  puts(utoa(uptime->us));
-  putch('\n');
+
+  //puts("total usecs is ");
+  //puts(utoa(uptime->us));
+  //putch('\n');
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
