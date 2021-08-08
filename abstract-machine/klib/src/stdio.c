f648bdf06a512daf116aeaee173f1f0c5054eb41
tracer-ics2020 <tracer@njuics.org>
2021-08-08 14:25:56 +0800

    >  run 191220000    Static hostname: zj          Icon name: computer-laptop            Chassis: laptop         Machine ID: ed65ba0b9ff84c5ebba30b86475a4861            Boot ID: e1da79a96dbc4bacb85264b6ec14cd98   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.4.0-81-generic       Architecture: x86-64  14:25:56 up 2 days, 18:56,  1 user,  load average: 1.06, 0.75, 0.43

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,109 +14,225 @@ int puts(const char *str) {
   return 0;
 }
 
-int printf(const char *fmt, ...) {
-  char ans[2048]={0};
-  va_list ap;
-  int ret = 0;
-  va_start(ap, fmt);
-  ret = vsprintf(ans, fmt, ap);
-  va_end(ap);
-  for(int i = 0; ans[i] != '\0';i++)
-  {
-    putch(ans[i]);
+#define MAX_BUF 2048
+
+#define is_digit(ch) ((ch) >= '0' && (ch) <= '9')
+
+#define do_dec                                                                 \
+  case 'd':                                                                    \
+  case 'D':                                                                    \
+    dec = va_arg(ap, int);                                                     \
+    out += print_num(out, dec, 10, len, upper_case, zero_filling);             \
+    break;
+
+#define do_percent                                                             \
+  case '%':                                                                    \
+    *out++ = *buf;                                                             \
+    break;                                                                     \
+
+#define do_hex                                                                 \
+  case 'X':                                                                    \
+    upper_case = true;                                                         \
+  case 'x':                                                                    \
+    hex = va_arg(ap, int);                                                     \
+    out += print_num(out, hex, 16, len, upper_case, zero_filling);             \
+    break;                                                                     \
+
+#define do_ch                                                                  \
+  case 'c':                                                                    \
+    _char = va_arg(ap, int);                                                   \
+    out += print_char(out, _char, len);                                        \
+    break;
+
+#define do_str                                                                 \
+  case 's':                                                                    \
+    p_char = va_arg(ap, char *);                                               \
+    out += print_str(out, p_char, len);                                        \
+    break;                                                                     \
+
+#define do_num                                                                 \
+  case '0':                                                                    \
+    zero_filling = true;                                                       \
+    buf++;                                                                     \
+  case '1':                                                                    \
+  case '2':                                                                    \
+  case '3':                                                                    \
+  case '4':                                                                    \
+  case '5':                                                                    \
+  case '6':                                                                    \
+  case '7':                                                                    \
+  case '8':                                                                    \
+  case '9':                                                                    \
+    while (is_digit(*buf)) {                                                   \
+      len = len * 10 + (*buf - '0');                                           \
+      buf++;                                                                   \
+    }                                                                          \
+    --buf;                                                                     \
+    goto again;
+
+#define do_oct                                                                 \
+  case 'o':                                                                    \
+  case 'O':                                                                    \
+    oct  = va_arg(ap, int);                                                    \
+    out += print_num(out, oct, 8, len, upper_case, zero_filling);              \
+    break;                                                                     \
+
+#define do_pointer                                                             \
+  case 'P':                                                                    \
+    upper_case = true;                                                         \
+  case 'p':                                                                    \
+    hex = va_arg(ap, int);                                                     \
+    out += print_num(out, hex, 16, len, upper_case, zero_filling);             \
+    break;                                                                     \
+
+#define CASE(func) do_##func break;
+
+const char upper_digits[36] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const char lower_digits[36] = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+static int print_num(char *buf, long long n, int base, int len, bool upper_case,
+                     bool zero) {
+  enum { MINUS, PLUS };
+  int real_len = 0;
+  int sym = n >= 0 ? PLUS : MINUS;
+  n = n >= 0 ? n : -n;
+  long long tmp = n;
+
+  do {
+    tmp /= base;
+    real_len++;
+  } while (tmp > 0);
+
+  if (len <= real_len) {
+    len = sym == MINUS ? real_len + 1 : real_len;
+  } 
+
+  char* workend = buf + len;
+  const char* digits = upper_case ? upper_digits : lower_digits;
+
+  do {
+    *--workend = digits[n % base];
+  } while ((n /= base) != 0);
+
+  if (zero) { 
+    while (workend != buf) {
+      *--workend = '0';
+    }
+    if (sym == MINUS)
+      *workend = '-';
+  } else {
+    if (sym == MINUS) {
+      *--workend = '-';
+    }
+    while (workend != buf) {
+      *--workend = ' ';
+    }
   }
-  return ret;
+
+  return len;
+}
+
+static int print_str(char *buf, char* p_str, int len) {
+  int str_len = strlen(p_str);
+  len = str_len > len ? str_len : len;
+  char* workend = buf + len;
+  char* str_end = p_str + str_len;
+  while (workend != buf && str_end != p_str) {
+    *--workend = *--str_end;
+  }
+  while (workend != buf)
+    *--workend = ' ';
+  return len;
+}
+
+static int print_char(char *buf, char ch, int len) {
+  len = len == 0 ? 1 : len;
+  char* workend = buf + len;
+  *--workend = ch;
+  while (workend != buf) {
+    *--workend = ' ';
+  }
+  return len;
+}
+
+int printf(const char *fmt, ...) {
+  char buf[MAX_BUF];
+  int res;
+  va_list arg;
+
+  va_start(arg, fmt);
+  res = vsprintf(buf, fmt, arg);
+  va_end(arg);
+  putstr(buf);
+  
+  return res;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  int length;
-  const char* a;
-  char *str=out;
-  for(;*fmt;++fmt){
-    if(*fmt!='%'){
-      *str++=*fmt;
-      continue;
+  const char *buf = fmt;
+  char* tmp = out;
+
+  while (*buf != '\0') {
+
+    bool zero_filling = false;
+    bool upper_case = false;
+    char _char = 0;
+    char *p_char = NULL;
+    int len = 0;
+    int dec = 0;
+    unsigned int hex = 0;
+    unsigned int oct = 0;
+
+    switch (*buf) {
+    case '%':
+    again:
+      switch (*++buf) {
+
+        CASE(percent) // %%
+
+        CASE(oct) // %[oO]
+
+        CASE(dec) // %[dD]
+
+        CASE(pointer) // %[pP]
+
+        CASE(hex) // %[xX]
+
+        CASE(ch) // %c
+
+        CASE(str) // %s
+
+        CASE(num)
+
+      default:
+        assert(0);
+        break;
+      }
+
+      break;
+    default:
+      *out++ = *buf;
+      break;
     }
-    ++fmt;
-    switch(*fmt){
-      case 's':{
-        a=va_arg(ap,char*);
-        length=strlen(a);
-        for(int i=0;i<length;i++){
-          *str++=*a++;
-        }
-        continue;
-      }
-      case 'd':{
-        int num=va_arg(ap,int);
-        int i=0;
-        char nums[20];
-        if(num==0)nums[0]='0';
-        else{
-          if(num<0){
-            *str++='-';
-            num=(-num);
-          }
-          while(num!=0){
-            nums[i++]=num%10+'0';
-            num/=10;
-          }
-        }
-        for(int j=i-1;j>=0;j--){
-          *str++=nums[j];
-        }
-        break;
-      }
-      case 'c':{
-        int num=va_arg(ap,int);
-        *str++=(char)num;
-        break;
-      }
-      case 'p':
-      case 'x':{
-        uint32_t num = va_arg(ap,uint32_t);
-        int i=0;
-        char nums[20];
-        if(num==0)nums[0]='0';
-        else{
-          while(num!=0){
-            uint32_t x=num%16;
-            if(x<10)nums[i++]=x+'0';
-            else nums[i++]=x-10+'a';
-            num/=16;
-          }
-        }
-        *str++='0';
-        *str++='x';
-        for(int j=i-1;j>=0;j--){
-          *str++=nums[j];
-        }
-        break;
-      }
-    
-      default:break;
-    }
+    buf++;
   }
-  *str='\0';
-  return 0;
-  //return 0;
+  *out = '\0';
+  return out - tmp;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
-  int ret = 0;
-  va_start(ap, fmt);
-  ret = vsprintf(out, fmt, ap);
-  va_end(ap);
-  return ret;
+  va_list arg;
+  int done;
+
+  va_start(arg, fmt);
+  done = vsprintf(out, fmt, arg);
+  va_end(arg);
+
+  return done;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  return 0;
-}
+int snprintf(char *out, size_t n, const char *fmt, ...) { return 0; }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  return 0;
-}
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) { return 0; }
 
 #endif
